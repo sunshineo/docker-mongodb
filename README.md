e6e22cf724b2db27a036d418bb954adefd0d2482
Gordon Sun <sunshineo@gmail.com>
2017-01-11 15:10:07 -0800

    Support ADMIN_DATABASE and ADMIN_USERNAME.

--- README.md
+++ README.md
@@ -7,9 +7,20 @@ https://github.com/docker-library/mongo/blob/4d7bd01562edefad38a240c40a4b162a1cd
 
 ##You can set admin password using an environment variable now
 ```
-docker run -it --rm -e ADMIN_PASSWORD=password sunshineo/mongo
+docker run -it --rm -e ADMIN_PASSWORD=password sunshineo/docker-mongodb
 ```
-##The username is 'admin'. I will make it a env var in future if I have time.
+This will create a user 'admin' in the 'admin' database with the password provided.
+
+## You can overwrite the admin database and admin username too
+```
+docker run -it --rm \
+-p 27017:27017 \
+-e ADMIN_DATABASE=myAdminDB \
+-e ADMIN_USERNAME=myAdmin \
+-e ADMIN_PASSWORD=password \
+sunshineo/docker-mongodb
+```
+But I highly suggest when you overwrite those, instead of using cmd line like above, use this image as base image and create your own docker image and simply export env variables
 
 ##Setting the password add about 15 seconds to start time.
 Read https://github.com/sunshineo/docker-mongodb/blob/master/new-entrypoint.sh and you will understand.
--- new-entrypoint.sh
+++ new-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+if [ -z "$ADMIN_DATABASE" ];
+then
+    echo "ADMIN_DATABASE not set. Use default 'admin'"
+    ADMIN_DATABASE='admin'
+fi
+
+if [ -z "$ADMIN_USERNAME" ];
+then
+    echo "ADMIN_USERNAME not set. Use default 'admin'"
+    ADMIN_USERNAME='admin'
+fi
+
 if [ -z "$ADMIN_PASSWORD" ];
 then
     echo "ADMIN_PASSWORD not set."
@@ -10,9 +22,9 @@ else
     echo "Sleep for 5 seconds to make sure mongod started."
     sleep 5
     echo "Remove existing admin user if already exists."
-    echo -e "use admin\n db.dropUser('admin');" | mongo
+    echo -e "use $ADMIN_DATABASE\n db.dropUser('$ADMIN_USERNAME');" | mongo
     echo "Set admin password."
-    echo -e "use admin\n db.createUser({ user: 'admin', pwd: '$ADMIN_PASSWORD', roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" } ] });" | mongo
+    echo -e "use $ADMIN_DATABASE\n db.createUser({ user: '$ADMIN_USERNAME', pwd: '$ADMIN_PASSWORD', roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" } ] });" | mongo
     echo "Admin password set. Stop mongod."
     pkill -f mongod
     echo "Sleep for 5 seconds to make sure mongod is stopped."
